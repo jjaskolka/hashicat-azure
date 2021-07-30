dfbdbfb1b7bfe487c8b60b376cce2471fb6fd68f
Jacob Jaskolka <jjaskolka@bhg-inc.com>
2021-07-30 18:03:21 +0000

    correct vnet file

--- vnet.tf
+++ vnet.tf
@@ -1,12 +1,4 @@
 module "network" {
-  source  = "app.terraform.io/jjaskolka-training/network/azurerm"
-  version = "3.0.1"
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  address_space       = "10.0.0.0/16"
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-  tags = {
-    Environment = "dev"
-    Cost-center = "it"
-  }
+  source  = "Azure/network/azurerm"
+  version = “3.0.1”
 }
